aa75f3c1e065c205396c1fa45b4dc7c5aea5ccef
MJT <masoudjt98@gmail.com>
2022-11-09 00:25:25 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Rabin_Encryption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenfileBTN = new System.Windows.Forms.Button();
+            this.InputTXTBX = new System.Windows.Forms.RichTextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.EncBTN = new System.Windows.Forms.Button();
+            this.ResultTXTBX = new System.Windows.Forms.RichTextBox();
+            this.SavefileBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OpenfileBTN
+            // 
+            this.OpenfileBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.OpenfileBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OpenfileBTN.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OpenfileBTN.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.OpenfileBTN.Location = new System.Drawing.Point(30, 33);
+            this.OpenfileBTN.Name = "OpenfileBTN";
+            this.OpenfileBTN.Size = new System.Drawing.Size(96, 77);
+            this.OpenfileBTN.TabIndex = 0;
+            this.OpenfileBTN.Text = "Open File";
+            this.OpenfileBTN.UseVisualStyleBackColor = true;
+            this.OpenfileBTN.Click += new System.EventHandler(this.OpenfileBTN_Click);
+            // 
+            // InputTXTBX
+            // 
+            this.InputTXTBX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.InputTXTBX.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTXTBX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(190)))), ((int)(((byte)(20)))));
+            this.InputTXTBX.Location = new System.Drawing.Point(142, 12);
+            this.InputTXTBX.Name = "InputTXTBX";
+            this.InputTXTBX.Size = new System.Drawing.Size(937, 142);
+            this.InputTXTBX.TabIndex = 1;
+            this.InputTXTBX.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // EncBTN
+            // 
+            this.EncBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.EncBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.EncBTN.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EncBTN.ForeColor = System.Drawing.Color.Lime;
+            this.EncBTN.Location = new System.Drawing.Point(487, 200);
+            this.EncBTN.Name = "EncBTN";
+            this.EncBTN.Size = new System.Drawing.Size(121, 42);
+            this.EncBTN.TabIndex = 2;
+            this.EncBTN.Text = "Encrypt";
+            this.EncBTN.UseVisualStyleBackColor = true;
+            this.EncBTN.Click += new System.EventHandler(this.EncBTN_Click);
+            // 
+            // ResultTXTBX
+            // 
+            this.ResultTXTBX.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(20)))), ((int)(((byte)(30)))));
+            this.ResultTXTBX.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultTXTBX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(190)))), ((int)(((byte)(20)))));
+            this.ResultTXTBX.Location = new System.Drawing.Point(142, 286);
+            this.ResultTXTBX.Name = "ResultTXTBX";
+            this.ResultTXTBX.Size = new System.Drawing.Size(937, 142);
+            this.ResultTXTBX.TabIndex = 3;
+            this.ResultTXTBX.Text = "";
+            // 
+            // SavefileBTN
+            // 
+            this.SavefileBTN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
+            this.SavefileBTN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SavefileBTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SavefileBTN.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SavefileBTN.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.SavefileBTN.Location = new System.Drawing.Point(30, 326);
+            this.SavefileBTN.Name = "SavefileBTN";
+            this.SavefileBTN.Size = new System.Drawing.Size(96, 77);
+            this.SavefileBTN.TabIndex = 4;
+            this.SavefileBTN.Text = "Save File";
+            this.SavefileBTN.UseVisualStyleBackColor = false;
+            this.SavefileBTN.Click += new System.EventHandler(this.SavefileBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(50)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(1091, 450);
+            this.Controls.Add(this.SavefileBTN);
+            this.Controls.Add(this.ResultTXTBX);
+            this.Controls.Add(this.EncBTN);
+            this.Controls.Add(this.InputTXTBX);
+            this.Controls.Add(this.OpenfileBTN);
+            this.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rabin Encryption";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenfileBTN;
+        private System.Windows.Forms.RichTextBox InputTXTBX;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button EncBTN;
+        private System.Windows.Forms.RichTextBox ResultTXTBX;
+        private System.Windows.Forms.Button SavefileBTN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Rabin_Encryption
+{
+    public partial class Form1 : Form
+    {
+        byte[] input;
+        byte[] result;
+        List<int> theQR = new List<int>() { 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 26, 61, 98, 137, 178, 221, 3, 50, 99, 150, 203, 258, 52, 111, 172, 235, 37, 104, 173, 244, 54, 129, 206, 22, 103, 186, 8, 95, 184, 12, 105, 200, 34, 133, 234, 74, 179, 23, 132, 243, 93, 208, 62, 181, 39, 162, 24, 151, 17, 148, 18, 153, 27, 166, 44, 187, 69, 216, 102, 253, 143, 35, 192, 88, 249, 149, 51, 218, 124, 32, 205, 117, 31, 210, 128, 48, 233, 157, 83, 11, 204, 136, 70, 6, 207, 147, 89, 33, 242, 190, 140, 92, 46, 2, 223, 183, 145, 109, 75, 43, 13, 248, 222, 198, 176, 156, 138, 122, 108, 96, 86, 78, 72, 68, 66 };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // Opening a file
+        private byte[] openFile()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog1.FileName;
+                byte[] fileBytes = File.ReadAllBytes(fileName);
+                return fileBytes;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // gets an integer and returns and array of the QR of it
+        private int[] getQR(int p)
+        {
+            List<int> Result = new List<int>();
+            Result.Add(0);
+            Result.Add(1);
+            for (int i = 2; i < p; i++)
+            {
+                if (!Result.Contains(Convert.ToInt32(Math.Pow(i, 2)) % p))
+                {
+                    Result.Add(Convert.ToInt32(Math.Pow(i, 2)) % p);
+                }
+            }
+            return Result.ToArray();
+        }
+        private void OpenfileBTN_Click(object sender, EventArgs e)
+        {
+            input = openFile();
+            result = new byte[input.Length];
+            if (input == null)
+                return;
+            StringBuilder sb = new StringBuilder();
+            input.ToList().ForEach(i => sb.Append(i + ","));
+            InputTXTBX.Text = sb.ToString();
+        }
+
+        private void EncBTN_Click(object sender, EventArgs e)
+        {
+            if(InputTXTBX.Text == "")
+            {
+                MessageBox.Show("There is nothing to encrypt");
+                return;
+            }
+            for(int i = 0; i < input.Length; i++)
+            {
+                result[i] = Convert.ToByte(encrypt(input[i]));
+            }
+            StringBuilder sb = new StringBuilder();
+            result.ToList().ForEach(i => sb.Append(i + ","));
+            ResultTXTBX.Text = sb.ToString();
+        }
+
+        private int encrypt(int result)
+        {
+            //int result = plain;
+            if (theQR.Contains(result))
+            {
+                result = Convert.ToInt32(Math.Pow(result, 2) % 263);
+                if (result > 255)
+                    return encrypt(result);
+                return result;
+            }
+            else
+            {
+                result = 263 - Convert.ToInt32(Math.Pow(result, 2) % 263);
+                if (result > 255)
+                    return encrypt(result);
+                return result;
+            }
+        }
+
+        private void SavefileBTN_Click(object sender, EventArgs e)
+        {
+            if (ResultTXTBX.Text == "")
+            {
+                MessageBox.Show("There is nothing to save");
+                return;
+            }
+
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|Image file(*.jpg)|*.jpg|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.Title = "Save The Selected text";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllBytes(saveFileDialog1.FileName, result);
+            }
+        }
+    }
+}
